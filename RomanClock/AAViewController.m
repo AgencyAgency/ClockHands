ad9cbc4cdc92ebc3b57f21e440c38d0e6a5d561d
Kyle Oba <koba@pasdechocolat.com>
2013-10-20 16:39:53 -1000

    Add seconds label and timer.

--- RomanClock/AAViewController.m
+++ RomanClock/AAViewController.m
@@ -7,23 +7,53 @@
 //
 
 #import "AAViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface AAViewController ()
-
+@property (strong, nonatomic) CADisplayLink *displayLink;
+@property (weak, nonatomic) IBOutlet UILabel *secondsLabel;
 @end
 
 @implementation AAViewController
 
+- (void)tick:(CADisplayLink *)sender
+{
+    // Calculate seconds of current minute with better than millisecond accuracy.
+    // 1/1,000,000th accuracy, I guess.
+    double secsDouble = [NSDate timeIntervalSinceReferenceDate];
+    int secsInt = (int)secsDouble;
+    int secPart = secsInt % 60;
+    double milliPart = secsDouble - secsInt;
+    double s = secPart + milliPart;
+    
+    self.secondsLabel.text = [NSString stringWithFormat:@":%02.03f", s];
+    
+    CGFloat percentage = s / 60.0;
+//    [self.secondsColorView changeColorForPercentage:percentage];
+//    [self.rgbColorView changeColorForPercentage:percentage];
+//    [self.grayscaleColorView changeColorForPercentage:percentage];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewWillAppear:animated];
+    
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(tick:)];
+    self.displayLink.frameInterval = 5; // 30 is about every half second.
+    [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
 }
 
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    
+    [self.displayLink invalidate];
+    self.displayLink = nil;
+}
 @end
